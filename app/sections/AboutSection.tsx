1c9afc04e7dee8ed2a47f2ea29ae73ff3e8874f0
Artur Melo <arturmcatunda@gmail.com>
2024-09-20 11:51:52 +0000

    now its fixed

--- app/sections/AboutSection.tsx
+++ app/sections/AboutSection.tsx
@@ -97,6 +97,7 @@ export default function AboutSection() {
           return (
             <motion.div
               className="flex"
+              key={i}
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
@@ -141,6 +142,7 @@ export default function AboutSection() {
             return (
               <motion.div
                 className="flex"
+                key={i}
                 initial={{ opacity: 0, y: 20 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true }}
