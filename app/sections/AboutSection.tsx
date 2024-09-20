cc9fd8164a1d18c846c930fa99096982c6223136
Artur Melo <arturmcatunda@gmail.com>
2024-09-20 11:50:43 +0000

    maybe its fixed now

--- app/sections/AboutSection.tsx
+++ app/sections/AboutSection.tsx
@@ -83,15 +83,20 @@ export default function AboutSection() {
     <div className="flex flex-col flex-grow justify-center items-center gap-4">
       <div className="flex flex-col flex-grow w-1/2 font-semibold text-2xl text-center justify-center items-center gap-4">
         {[
-          <p>{`Meu nome é Artur Melo Catunda.`}</p>,
-          <p>{`Sou Desenvolvedor Web especializado em React.js, React Native e Tailwind CSS, mas com sólida experiência também em back-end utilizando Express e Spring. Também tenho familiaridade com o uso de Docker, GitHub Actions e AWS para criação de ambientes de desenvolvimento e hospedagem.`}</p>,
-          <p>{`Trabalho com metodologias ágeis e boas práticas de desenvolvimento. Minhas principais habilidades incluem foco, resiliência, comprometimento, organização e solução de problemas, características que sempre aplico em meus projetos para entregar soluções eficientes e bem estruturadas.`}</p>,
-          <p>{`No meu tempo livre, adoro desenvolver jogos, assistir filmes de terror e jogar jogos de tabuleiro e cartas. Se precisar de um currículo ou quiser bater um papo, fico à disposição!`}</p>,
+          <p key={0}>{`Meu nome é Artur Melo Catunda.`}</p>,
+          <p
+            key={1}
+          >{`Sou Desenvolvedor Web especializado em React.js, React Native e Tailwind CSS, mas com sólida experiência também em back-end utilizando Express e Spring. Também tenho familiaridade com o uso de Docker, GitHub Actions e AWS para criação de ambientes de desenvolvimento e hospedagem.`}</p>,
+          <p
+            key={2}
+          >{`Trabalho com metodologias ágeis e boas práticas de desenvolvimento. Minhas principais habilidades incluem foco, resiliência, comprometimento, organização e solução de problemas, características que sempre aplico em meus projetos para entregar soluções eficientes e bem estruturadas.`}</p>,
+          <p
+            key={3}
+          >{`No meu tempo livre, adoro desenvolver jogos, assistir filmes de terror e jogar jogos de tabuleiro e cartas. Se precisar de um currículo ou quiser bater um papo, fico à disposição!`}</p>,
         ].map((v, i) => {
           return (
             <motion.div
               className="flex"
-              key={i}
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
@@ -104,21 +109,25 @@ export default function AboutSection() {
         <div className="flex flex-row justify-center items-center gap-8 mt-8">
           {[
             <AboutSectionIconButton
+              key={0}
               icon="source"
               size="30"
               link="https://github.com/Artotz"
             />,
             <AboutSectionIconButton
+              key={1}
               icon="linkedin"
               size="30"
               link="https://www.linkedin.com/in/arturmcatunda/"
             />,
             <AboutSectionIconButton
+              key={2}
               icon="mail"
               size="30"
               link="mailto:arturmcatunda@gmail.com"
             />,
             <a
+              key={3}
               href="curriculo.pdf"
               download="Currículo - Artur Melo Catunda.pdf"
               className={
@@ -132,7 +141,6 @@ export default function AboutSection() {
             return (
               <motion.div
                 className="flex"
-                key={i}
                 initial={{ opacity: 0, y: 20 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true }}
