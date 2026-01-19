a5654bd7cb7a6264f0b90ff9619bd3eaeb60a8d3
Artur Catunda <arturmcatunda@gmail.com>
2026-01-19 08:59:48 -0300

    i18n fixes

---
+++ types/i18n.d.ts
@@ -0,0 +1,23 @@
+declare module "i18n" {
+  type I18nCatalogValue = string | I18nCatalog;
+  interface I18nCatalog {
+    [key: string]: I18nCatalogValue;
+  }
+
+  interface I18nConfigureOptions {
+    locales: string[];
+    defaultLocale?: string;
+    staticCatalog?: Record<string, I18nCatalog>;
+    objectNotation?: boolean | string;
+    updateFiles?: boolean;
+  }
+
+  interface I18nApi {
+    configure(options: I18nConfigureOptions): void;
+    setLocale(locale: string): void;
+    getCatalog(locale?: string): I18nCatalog;
+  }
+
+  const i18n: I18nApi;
+  export default i18n;
+}
